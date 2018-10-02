4a4d19545bf92603d7b8672a354df6adc5faea33
Darwin Boaventura <darwinboaventura@gmail.com>
2018-10-02 18:10:40 -0300

    Iniciado a montagem de alguns components

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,10 @@ const merge = require('merge-stream');
 const buffer = require('vinyl-buffer');
 const connect = require('gulp-connect');
 const clean = require('gulp-clean');
+const runSequence = require('run-sequence');
 
 gulp.task('sass', function() {
-	gulp.src('./src/sass/app.scss')
+	return gulp.src('./src/sass/**/*.scss')
 	.pipe(sass({
 		outputStyle: 'compressed'
 	}).on('errors', sass.logError))
@@ -23,7 +24,7 @@ gulp.task('sass', function() {
 });
 
 gulp.task('js', function() {
-	gulp.src('./src/js/app.js')
+	return gulp.src('./src/js/app.js')
 	.pipe(minify({
 		noSource: true
 	}))
@@ -32,7 +33,7 @@ gulp.task('js', function() {
 });
 
 gulp.task('images:copy:optmization', function() {
-	gulp.src('./src/images/*')
+	return gulp.src('./src/images/*')
 	.pipe(imagemin())
 	.pipe(gulp.dest('./dist/images'))
 	.pipe(connect.reload());
@@ -42,53 +43,55 @@ gulp.task('icons:sprite:optmization', function() {
 	const spriteData = gulp.src('./src/icons/**/*.png')
 	.pipe(spritesmith({
 		imgName: 'icons-sprite.png',
-		cssName: 'icons-sprite.scss'
+		cssName: 'icons-sprite.css'
 	}));
 
-	if (spriteData.img && spriteData.css) {
-		const iconStream = spriteData.img
-		.pipe(buffer())
-		.pipe(imagemin())
-		.pipe(gulp.dest('./src/sass/sprite'));
+	const iconStream = spriteData.img
+	.pipe(buffer())
+	.pipe(imagemin())
+	.pipe(gulp.dest('./dist/css/sprite'));
 
-		const sassStream = spriteData.css
-		.pipe(gulp.dest('./src/sass/sprite'));
+	const sassStream = spriteData.css
+	.pipe(gulp.dest('./dist/css/sprite'));
 
-		return merge(iconStream, sassStream)
-		.pipe(connect.reload());
-	}
+	return merge(iconStream, sassStream).pipe(connect.reload());;
 });
 
 gulp.task('html:copy', function() {
-	gulp.src('./src/**/*.html')
+	return gulp.src('./src/**/*.html')
 	.pipe(gulp.dest('./dist'))
 	.pipe(connect.reload());
 });
 
 gulp.task('server', function() {
-	connect.server({
+	return connect.server({
 		root: 'dist',
 		livereload: true
 	});
 });
 
 gulp.task('dist:clean', function() {
-	gulp.src('./dist', {
+	return gulp.src('./dist', {
 		read: false
 	})
-	.pipe(clean());
+	.pipe(clean())
+	.pipe(connect.reload());
 });
 
 gulp.task('watch', function() {
 	gulp.watch('./src/js/app.js', ['js']);
 	gulp.watch('./src/**/*.html', ['html:copy']);
-	gulp.watch('./src/sass/app.scss', ['sass']);
 	gulp.watch('./src/images/*', ['images:copy:optmization']);
 	gulp.watch('./src/icons/**/*.png', ['icons:sprite:optmization']);
+	gulp.watch('./src/sass/**/*.scss', ['sass']);
 });
 
-gulp.task('build', ['dist:clean', 'js', 'html:copy', 'icons:sprite:optmization', 'images:copy:optmization', 'sass'], function() {
-	console.log('Build executado com sucesso, seus arquivos estão na pasta ./dist');
+gulp.task('build', function() {
+	return runSequence('dist:clean', 'js', 'images:copy:optmization', 'icons:sprite:optmization', 'sass', 'html:copy', function() {
+		console.log('Finalizou o build');
+	});
 });
 
-gulp.task('default', ['server', 'watch']);
+gulp.task('default', function() {
+	return runSequence('build', 'watch', 'server');
+});
