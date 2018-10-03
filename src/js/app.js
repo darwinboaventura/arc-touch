42ce409335642f2a41f8b3c2b613a85321af8fba
Darwin Boaventura <darwinboaventura@gmail.com>
2018-10-03 20:09:07 -0300

    Refactor JS

--- src/js/app.js
+++ src/js/app.js
@@ -1,60 +1,125 @@
-function calculateParentWidth() {
-	const el = $('[data-item-to-calculate]');
-	const width = el.parents('[data-item-to-calculate-parent]').width();
+function CalculateHelper(params) {
+	this.identifier = params.identifier;
+	this.parentIdentifier = params.parentIdentifier;
 
-	el.width(width);
+	this.calculateWidthOfParent = function() {
+		return $(this.identifier).parents(this.parentIdentifier).width();
+	};
+
+	this.setElementWidth = function(width) {
+		return $(this.identifier).width(width);
+	}
+
+	this.setElementWidthBasedOnParent = function() {
+		const width = this.calculateWidthOfParent();
+
+		this.setElementWidth(width);
+	};
 }
 
-function handleCarousel() {
-	const container = $('.comp-carousel');
-	const containerWidth = container.width();
-	const listOfBanners = $('.comp-carousel-list');
-	const totalItems = container.find('.comp-carousel-list-item').length;
-	const totalWidth = containerWidth * totalItems;
-	
-	listOfBanners.width(totalWidth);
+function CarouselSlider(params) {
+	this.containerIdentifier = params.containerIdentifier;
+	this.listIdentifier = params.listIdentifier;
+	this.itemIdentifier = params.itemIdentifier;
+	this.bulletItemIdentifier = params.bulletItemIdentifier;
+	this.nameActiveClass = params.nameActiveClass;
 
-	let activeBullet = 0;
-	let leftPosition = 0;
+	this.state = {
+		bullet: 0,
+		leftPosition: 0,
+		rotate: null
+	};
 
-	function handleBulletActivation(activeBullet) {		
-		$('.comp-carousel-bullets-item').eq(activeBullet).addClass('is-active').siblings('.comp-carousel-bullets-item').removeClass('is-active');
+	this.calculateWidth = function(identifier) {
+		return $(identifier).width();
+	};
+
+	this.calculateQuantityElements = function(searchAt, identifier) {
+		return $(searchAt).find(identifier).length;
 	}
 
-	function handleBannerActivation(leftPosition) {
-		listOfBanners.css('left', '-' + leftPosition + 'px');
+	this.calculateWidthOfList = function(containerIdentifier, itemIdentifier) {
+		const width = this.calculateWidth(containerIdentifier);
+		const quantity = this.calculateQuantityElements(containerIdentifier, itemIdentifier);
+
+		return width * quantity;
+	};
+
+	this.setWidthOfList = function() {
+		const width = this.calculateWidthOfList(this.containerIdentifier, this.itemIdentifier);
+
+		$(this.listIdentifier).width(width);
 	}
 
-	function handleBulletNavigation() {
-		$('.comp-carousel-bullets-item').click(function() {
-			const index = $(this).index();
+	this.setActiveBullet = function() {
+		$(this.bulletItemIdentifier).eq(this.state.bullet).addClass(this.nameActiveClass).siblings(this.bulletItemIdentifier).removeClass(this.nameActiveClass);
+	}
 
-			handleBannerActivation(containerWidth * index);
-			handleBulletActivation(index);
+	this.setLeftPositionBanner = function() {
+		$(this.listIdentifier).css('left', '-' + this.state.leftPosition + 'px');
+	}
 
-			clearInterval(sliderProgress);
+	this.stopAutoRotate = function() {
+		return clearInterval(this.state.rotate);
+	}
+
+	this.handleBulletNavigation = function() {
+		const self = this;
+
+		$(this.bulletItemIdentifier).click(function() {
+			const containerWidth = self.calculateWidth(self.containerIdentifier);
+
+			self.state.bullet = $(this).index();
+			self.state.leftPosition = containerWidth * self.state.bullet;
+
+			self.setLeftPositionBanner();
+			self.setActiveBullet();
+
+			self.stopAutoRotate();
 		});
+	};
+
+	this.handleAutoRotate = function() {
+		const self = this;
+
+		this.state.rotate = setInterval(function() {
+			let widthAfter = self.state.leftPosition + self.calculateWidth(self.containerIdentifier);
+			let widthOfList = self.calculateWidthOfList(self.containerIdentifier, self.itemIdentifier);
+
+			if (widthAfter < widthOfList) {
+				self.state.leftPosition += self.calculateWidth(self.containerIdentifier);
+			} else {
+				self.state.leftPosition = 0;
+			}
+
+			self.state.bullet = self.state.leftPosition / self.calculateWidth(self.containerIdentifier);
+
+			self.setActiveBullet();
+			self.setLeftPositionBanner();
+		}, 4000);
 	}
+};
 
-	const sliderProgress = setInterval(function() {
-		if ((leftPosition + containerWidth) < totalWidth) {
-			leftPosition += containerWidth;
-		} else {
-			leftPosition = 0;
-		}
-
-		handleBannerActivation(leftPosition);
-		handleBulletActivation((leftPosition / containerWidth));		
-	}, 4000);
-
-	handleBulletNavigation();
-}
+const carouselHelper = new CalculateHelper({
+	identifier: '[data-item-to-calculate]',
+	parentIdentifier: '[data-item-to-calculate-parent]'
+});
 
 $(document).ready(function() {
-	calculateParentWidth();
-	handleCarousel();
+	carouselHelper.setElementWidthBasedOnParent();
+
+	const testimonies = new CarouselSlider({
+		containerIdentifier: '.comp-carousel',
+		listIdentifier: '.comp-carousel-list',
+		itemIdentifier: '.comp-carousel-list-item',
+		bulletItemIdentifier: '.comp-carousel-bullets-item',
+		nameActiveClass: 'is-active'
+	});
+
+	testimonies.handleAutoRotate();
+	testimonies.handleBulletNavigation();
 });
 
 $(window).resize(function() {
-	calculateParentWidth();
+	carouselHelper.setElementWidthBasedOnParent();
 });
