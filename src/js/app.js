1758830ab4fda584cd4faf8bc6a29c91cf660290
Darwin Boaventura <darwinboaventura@gmail.com>
2018-10-03 02:52:03 -0300

    Implementado JS do carousel

--- src/js/app.js
+++ src/js/app.js
@@ -5,8 +5,54 @@ function calculateParentWidth() {
 	el.width(width);
 }
 
+function handleCarousel() {
+	const container = $('.comp-carousel');
+	const containerWidth = container.width();
+	const listOfBanners = $('.comp-carousel-list');
+	const totalItems = container.find('.comp-carousel-list-item').length;
+	const totalWidth = containerWidth * totalItems;
+	
+	listOfBanners.width(totalWidth);
+
+	let activeBullet = 1;
+	let leftPosition = containerWidth;
+
+	function handleBulletActivation(activeBullet) {		
+		$('.comp-carousel-bullets-item').eq(activeBullet).addClass('is-active').siblings('.comp-carousel-bullets-item').removeClass('is-active');
+	}
+
+	function handleBannerActivation(leftPosition) {
+		listOfBanners.css('left', '-' + leftPosition + 'px');
+	}
+
+	function handleBulletNavigation() {
+		$('.comp-carousel-bullets-item').click(function() {
+			const index = $(this).index();
+
+			handleBannerActivation(containerWidth * index);
+			handleBulletActivation(index);
+
+			clearInterval(sliderProgress);
+		});
+	}
+
+	const sliderProgress = setInterval(function() {
+		if ((leftPosition + containerWidth) < totalWidth) {
+			leftPosition += containerWidth;
+		} else {
+			leftPosition = containerWidth;
+		}
+
+		handleBannerActivation(leftPosition);
+		handleBulletActivation((leftPosition / containerWidth) - 1);		
+	}, 4000);
+
+	handleBulletNavigation();
+}
+
 $(document).ready(function() {
 	calculateParentWidth();
+	handleCarousel();
 });
 
 $(window).resize(function() {
